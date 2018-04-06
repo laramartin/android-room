3e3e448e043b4c32f36db62131d02bf522472901
laramartin <lmartin@babbel.com>
2018-04-06 20:07:19 +0200

    Use Kotlin annotation processor

--- app/src/main/java/eu/laramartin/room/model/Task.kt
+++ app/src/main/java/eu/laramartin/room/model/Task.kt
@@ -5,6 +5,6 @@ import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "tasks")
 data class Task(
-        @PrimaryKey(autoGenerate = true) private val id: Long,
+        @PrimaryKey(autoGenerate = true) val id: Long,
         val description: String,
         val status: Int)
